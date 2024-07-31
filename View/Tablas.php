37c5c7e38671f92a6b1d0adb0c87668125751699
Mamorenobob <161890372+Mamorenobob@users.noreply.github.com>
2024-07-31 17:27:29 -0500

    Update Tablas.php

--- View/Tablas.php
+++ View/Tablas.php
@@ -6,51 +6,43 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 
+    <?php 
+    session_start();
+
+    include '../Controller/conexion.php';
+    ?>
+
     <style>
         body {
     background-color: #f0f0f0; /* Color gris claro para el fondo de toda la página */
     background: linear-gradient(to bottom, #000000, #7BB4E3);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
 }
-        table {
-            color:white;
-            border: 1px solid black;
-            overflow-y: scroll; /* Activa el desplazamiento vertical */
-            border-collapse: collapse; /* Combina los bordes para que no se dupliquen */
-        }
-        th, td {
-            background:black;
-            border: 1px solid black; /* Bordes internos */
-            padding: 8px; /* Espaciado interno de las celdas */
-            text-align: left; /* Alineación del texto */
-        }
-        h1{ 
-           
-        }
-        <style>
-        .up-and-down {
-            width: 100px;
-            height: 100px;
-            background-color: #3498db;
-            display: inline-block;
-            animation: upAndDown 2s infinite;
-        }
-
-        @keyframes upAndDown {
-            0% { transform: translateY(0); }
-            50% { transform: translateY(-20px); }
-            100% { transform: translateY(0); }
-        }
-        .registro {
-    display: none; /* Oculto por defecto */
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5); /* Efecto de "grisar" */
-    justify-content: center;
-    align-items: center;
+.todo{
+    background-color: white;
+    border: 10px;
+    width: 450px;
+    height: 750px;
+    display: none;
 }
+    .contenedor{
+        width: 400px;
+        backdrop-filter: blur(15px);
+        background-color: rgba(0,0,0,0.5);
+        background: linear-gradient(to right, #000000, #7BB4E3);
+        height: 720px;
+        width: 400px;
+        text-align: center;
+        border-radius: 10px;
+        color: #f0f0f0;
+    }
+    .hidden {
+    display: none;
+}
+    
     </style>
 </head>
 <body>
@@ -58,88 +50,52 @@
 <div class="container mt-5 position.relative">
         <div class="row">
             <div class="col">
-                <center>
-            <tr>REGISTRO PRODUCTO</tr>
+            <form action="#" method="post" class registro>
+            <button id="toggleButton">Donde</button>
+            </form>
+    <center>
+<div class="todo">
+    <div id="formContainer" class="hidden">
+            <form action="#" method="post" class registro>
+                
+                        <label for="">Nombre:</label>
+                        <input type="text" class="form-control">
+                        <label for="">cantidad:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">proveedor:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">valor:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">ubicacion:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">fecha:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">marca:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">codigo:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">codigo_de_barras:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">descripcion:</label>
+                        <input type="numero" class="form-control w-25">
+                        <label for="">categoria:</label>
+                        <input type="numero" class="form-control w-25">
+                    <input type="submit" name="accion" value="agregar producto">
+                    <input type="submit" name="accion" value="eliminar">
+                    <input type="submit" name="accion" value="editar">
+                    
+            </form>
+    </div>
+</div>
 </center>
-<table class="table table-dark" id="table">
-                    <tr>
-                        <th>id</th>
-                        <th>Nombre</th>
-                        <th>cantidad</th>
-                        <th>proveedor</th>
-                        <th>valor</th>
-                        <th>ubicacion</th>
-                        <th>fecha</th>
-                        <th>marca</th>
-                        <th>codigo</th>
-                        <th>codigo_de_barras</th>
-                        <th>descripcion</th>
-                        <th>categoria</th>
-                    </tr>
 
-            <form action="#" method="post">
-        <table>
-            <td>
-                <div class="text-center"> resgistro producto</div>
-            <div class="form-control borde-white">
-                <label for="">Nombre:</label>
-                <input type="text" class="form-control">
-            </div>
-            <div class="form-control border-white">
-                <label for="">cantidad:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            </div>
-            <div class="form-control border-white">
-                <label for="">proveedor:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">valor:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">ubicacion:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">fecha:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">marca:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">codigo:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">codigo_de_barras:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">descripcion:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-            <div class="form-control border-white">
-                <label for="">categoria:</label>
-                <input type="numero" class="form-control w-25">
-            </div>
-    <input type="submit" name="accion" value="agregar producto">
-    <input type="submit" name="accion" value="eliminar">
-    <input type="submit" name="accion" value="editar">
-</td>
-</form>
-</table>
-    
 
 
 
 
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+/*if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $accion = $_POST['accion'];
     switch ($accion) {
         case 'agregar producto':
@@ -166,13 +122,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         case 'editar':
             // Lógica para editar
             break;
+        case 'agregar':
+            break;
     }
-}
+}*/
 ?>
-
-
-
- 
-
 </body>
 </html>
+<script>
+    document.getElementById('toggleButton').addEventListener('click', function() {
+        const formContainer = document.getElementById('formContainer');
+        
+        if (formContainer.classList.contains('hidden')) {
+            formContainer.classList.remove('hidden');
+            this.textContent = 'Ocultar Formulario';
+        } else {
+            formContainer.classList.add('hidden');
+            this.textContent = 'Mostrar Formulario';
+        }
+    });
+    </script>
