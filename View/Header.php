a7584ac6440dafe42d6ecfa0eba8e7acfcd08ec8
AndersonMora <161890189+AndersonMora12@users.noreply.github.com>
2024-08-01 14:31:21 -0500

    1
    logo
    
    Co-Authored-By: ZDELMAXZ <163585006+ZDELMAXZ@users.noreply.github.com>
    Co-Authored-By: Subasa1012 <176712410+Subasa1012@users.noreply.github.com>
    Co-Authored-By: Mamorenobob <161890372+Mamorenobob@users.noreply.github.com>
    Co-Authored-By: DiegoD330 <163585055+DiegoD330@users.noreply.github.com>

--- View/Header.php
+++ View/Header.php
@@ -5,10 +5,12 @@
         margin: 0;
         backdrop-filter: blur(10px);
         background-color: rgba(175, 122, 197, 0.5);
+        
     }
     img{
         width: 80px;
         margin: 10px;
+        border-radius: 50px;
     }
 </style>
 <header>
