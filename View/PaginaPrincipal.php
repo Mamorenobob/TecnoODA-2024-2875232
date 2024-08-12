57651fe7951ed7820516515d3fdcc318739d77df
Mamorenobob <161890372+Mamorenobob@users.noreply.github.com>
2024-08-12 13:36:28 -0500

    Update PaginaPrincipal.php

--- View/PaginaPrincipal.php
+++ View/PaginaPrincipal.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="PStyleD.css">
-    <link rel="stylesheet" href="styleD.css">
+    <link rel="stylesheet" href="../Model/Css/styleD.css">
+    <link rel="stylesheet" href="../Model/Css/PStyleD.css">
     <link rel="icon" href="imagenes/logo.png">
     <title>Tecno ODA</title>
     </head>
