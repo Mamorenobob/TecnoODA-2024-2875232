62a9e81dea5ad8fdc7017a93d691135f57a458c6
Anderson Mora <161890189+AndersonMora12@users.noreply.github.com>
2024-08-12 14:03:35 -0500

    Update Header.php

--- View/Header.php
+++ View/Header.php
@@ -17,7 +17,4 @@ img{
     <div class="header">
         <img src="../Images/logo.png" alt="">
     </div>
-    <div id="header-img">
-        <img src="../Images/13.gif" alt="" style="width: 500px; height:auto; float: right; ">
-    </div>
 </header>
