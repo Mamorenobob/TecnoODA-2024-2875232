5a37a5b4a0b3d0886983036d1539fe5936e5850c
DiegoD330 <diegodmoreno330@gmail.com>
2024-08-12 00:23:25 -0500

    Actualizacion Pagina Principal
    Rehice el header y puse el boton de acciones

--- View/PaginaPrincipal.php
+++ View/PaginaPrincipal.php
@@ -1,74 +1,38 @@
-<?php
-    session_start();
-
-    if(!isset($_SESSION['Usuario'])){
-        echo'
-            <script>
-                alert("Por favor debes inicar sesion");
-                window.location = "LoginRegister.php";
-            </script>
-        ';
-        session_destroy();
-        die();
-    }
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Model/Css/styleD.css">
+    <link rel="stylesheet" href="PStyleD.css">
+    <link rel="stylesheet" href="styleD.css">
     <link rel="icon" href="imagenes/logo.png">
     <title>Tecno ODA</title>
-</head>
+    </head>
 <body>
     <header id="arriba" class="parallax">
         <div id="header-img">
-            <img src="../Images/logo.png">
-            </style>
-    
+            <img src="imagenes/logo.png">
         </div>
-            <div class="exp-text">
+        <div class="exp-text">
             <div>
                 <h1>Tecno ODA</h1>
                 <h3>2024</h3>
             </div>
-        
-        </div >  
-        <div id="header-img">
-        <img src="../Images/13.gif" alt="" style="float: right;">
         </div>
-       
     </header>
-    
 
-
-
-
-    <nav id="menu">
-        <ul>
-            <li><a href="#menu">&#9776;</a></li>
-            <li><a href="#">x</a></li>
-            <li><a href="#productos">Productos</a></li>
-            <li><a href="../View/cerrar_sesion.php"><button>Cerrar Sesion</button></a></li>
-        </ul>
-    </nav>
-
-
-
-
-   
-    </div>
-
-
-
+    <ul class="nav">
+                <li><a href="">Productos</a></li>
+            </ul>
+        <button id="botonPrincipal">opciones</button>
+        <div id="botonesDesplegables" class="oculto">
+        <button><li><a href="../View/cerrar_sesion.php">Cerrar sesión</a></li></button>
+        <button>Opción 2</button>
+        </div>
 
     <aside id="educacion">
-        <br>
         <hr>
-        <ul>
-            <li>
                 <article>
                     <div class="edu-img">
                         <img src="imagenes/Producto1.jpeg">
@@ -79,84 +43,7 @@
                         <p class="descripcion">descripcion</p>
                     </div>
                 </article>
-            </li>  
-
-
-
-
-            <li>
-                <article>
-                    <div class="edu-img">
-                        <img src="imagenes/producto2.jpeg">
-                    </div>
-                    <div class="edu-text">
-                        <p class="periodo">Tipo de producto</p>
-                        <p class="titulo">Producto</p>
-                        <p class="descripcion">descripcion</p>
-                    </div>
-                </article>
-            </li>  
-
-
-
-
-            <li>
-                <article>
-                    <div class="edu-img">
-                        <img src="imagenes/producto3.jpeg">
-                    </div>
-                    <div class="edu-text">
-                        <p class="periodo">Tipo de producto</p>
-                        <p class="titulo">Producto</p>
-                        <p class="descripcion">descripcion</p>
-                    </div>
-                </article>
-            </li>  
-
-
-
-
-            <li>
-                <article>
-                    <div class="edu-img">
-                        <img src="imagenes/producto4.jpeg">
-                    </div>
-                    <div class="edu-text">
-                        <p class="periodo">Tipo de producto</p>
-                        <p class="titulo">Producto</p>
-                        <p class="descripcion">descripcion</p>
-                    </div>
-                </article>
-            </li>  
-
-
-
-
-            <li>
-                <article>
-                    <div class="edu-img">
-                        <img src="imagenes/producto5.jpeg">
-                    </div>
-                    <div class="edu-text">
-                        <p class="periodo">Tipo de producto</p>
-                        <p class="titulo">Producto</p>
-                        <p class="descripcion">descripcion</p>
-                    </div>
-                </article>
-            </li>  
-        </ul>
     </aside>
-    
-        
-
-
-
-     </section>
-      
-      
-                </article>              
-            </li>                                  
-        </ul>
-    </div>
-
-
+</body>
+<script src="boton.js"></script>
+</html>
