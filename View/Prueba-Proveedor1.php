e55265eca95bd53340b626aa4cb0ece2987b94ff
Mamorenobob <161890372+Mamorenobob@users.noreply.github.com>
2024-08-17 16:19:47 -0500

    Update Prueba-Proveedor1.php

--- View/Prueba-Proveedor1.php
+++ View/Prueba-Proveedor1.php
@@ -122,8 +122,9 @@
                         <a class="carousel-control-prev" href="#carrusel" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Anterior</span>
-                        </a>
+                            </a>
                         <a class="carousel-control-next" href="#carrusel" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Siguiente</span>
                         </a>
+                            <span class="sr-only">Siguiente</span>
